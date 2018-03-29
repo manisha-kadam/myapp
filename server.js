c62eb05265539897f3cba8814a964382c12d26ae
gslab <manisha.kadam@gslab.com>
2018-03-29 17:30:36 +0530

    new project

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.post("/login", function (req, res) {
 
   console.log(req.body);
 
-  login.get('username', function(err, data) {
-    console.log(`Document contents: ${data.toString('utf8')}`);
-  });
+//   mydb.get('username', function(err, data) {
+//     console.log(`Document contents: ${data.toString('utf8')}`);
+//   });
 
 res.send("Hello " + username);
     
@@ -42,41 +42,41 @@ res.send("Hello " + username);
 
 
 // load local VCAP configuration  and service credentials
-var vcapLocal;
-try {
-  vcapLocal = require('./vcap-local.json');
-  console.log("Loaded local VCAP", vcapLocal);
-} catch (e) { }
+// var vcapLocal;
+// try {
+//   vcapLocal = require('./vcap-local.json');
+//   console.log("Loaded local VCAP", vcapLocal);
+// } catch (e) { }
 
-const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+// const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
 
-const appEnv = cfenv.getAppEnv(appEnvOpts);
+// const appEnv = cfenv.getAppEnv(appEnvOpts);
 
-if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
-  // Load the Cloudant library.
-  var Cloudant = require('cloudant');
+// if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
+//   // Load the Cloudant library.
+//   var Cloudant = require('cloudant');
 
-  // Initialize database with credentials
-  if (appEnv.services['cloudantNoSQLDB']) {
-     // CF service named 'cloudantNoSQLDB'
-     var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
-  } else {
-     // user-provided service with 'cloudant' in its name
-     var cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
-  }
+//   // Initialize database with credentials
+//   if (appEnv.services['cloudantNoSQLDB']) {
+//      // CF service named 'cloudantNoSQLDB'
+//      var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+//   } else {
+//      // user-provided service with 'cloudant' in its name
+//      var cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+//   }
 
-  //database name
-  var dbName = 'login';
+//   //database name
+//   var dbName = 'mydb';
 
-  // Create a new "mydb" database.
-  cloudant.db.create(dbName, function(err, data) {
-    if(!err) //err if database doesn't already exists
-      console.log("Created database: " + dbName);
-  });
+//   // Create a new "mydb" database.
+//   cloudant.db.create(dbName, function(err, data) {
+//     if(!err) //err if database doesn't already exists
+//       console.log("Created database: " + dbName);
+//   });
 
-  // Specify the database we are going to use (mydb)...
-  database = cloudant.db.use(dbName);
-}
+//   // Specify the database we are going to use (mydb)...
+//   database = cloudant.db.use(dbName);
+// }
 
 
 //serve static file (index.html, images, css)
